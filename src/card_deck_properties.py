5c81bbb504bbf6bb1834a3dedf251db3ad6f6a50
ijgnd <fooijgnd@barijgnd>
2020-05-15 00:19:05 +0200

    use get ?

--- src/card_deck_properties.py
+++ src/card_deck_properties.py
@@ -16,7 +16,7 @@ def current_card_deck_properties(card):
         source_deck_name = ""
 
     formatted_steps = ''
-    for i in conf['new']['delays']:
+    for i in conf.get('new', {}).get('delays', []):
         formatted_steps += ' -- ' + mw.col.backend.format_time_span(i * 60)
 
 
@@ -71,39 +71,39 @@ def current_card_deck_properties(card):
     p["now"] = time.strftime('%Y-%m-%d %H:%M', time.localtime(card.id/1000))
     p["conf"] = conf
     # Deck Options
-    p["d_OptionGroupID"] = conf['id']
-    p["d_OptionGroupName"] = conf['name']
-    p["d_OptionGroupName_fmt"] = fmt_long_string(conf['name'], gc('optiongroup_names_length', 20))
-    p["d_IgnoreAnsTimesLonger"] = conf["maxTaken"]
-    p["d_ShowAnswerTimer"] = conf["timer"]
-    p["d_Autoplay"] = conf["autoplay"]
-    p["d_Replayq"] = conf["replayq"]
-    p["d_IsDyn"] = conf["dyn"]
-    p["d_usn"] = conf["usn"]
-    p["d_mod"] = conf["mod"]
-    p["d_new_steps"] = conf['new']['delays']
-    p["d_new_steps_str"] = str(conf['new']['delays'])
+    p["d_OptionGroupID"] = conf.get('id',"")
+    p["d_OptionGroupName"] = conf.get('name', "")
+    p["d_OptionGroupName_fmt"] = fmt_long_string(conf.get('name',""), gc('optiongroup_names_length', 20))
+    p["d_IgnoreAnsTimesLonger"] = conf.get("maxTaken", "")
+    p["d_ShowAnswerTimer"] = conf.get("timer", "")
+    p["d_Autoplay"] = conf.get("autoplay", "")
+    p["d_Replayq"] = conf.get("replayq", "")
+    p["d_IsDyn"] = conf.get("dyn", "")
+    p["d_usn"] = conf.get("usn", "")
+    p["d_mod"] = conf.get("mod", "")
+    p["d_new_steps"] = conf.get('new', {}).get('delays')
+    p["d_new_steps_str"] = str(conf.get('new', {}).get('delays', ""))
     p["d_new_steps_fmt"] = formatted_steps
-    p["d_new_order"] = conf['new']['order']
-    p["d_new_NewPerDay"] = conf['new']['perDay']
-    p["d_new_GradIvl"] = str(conf['new']['ints'][0])
-    p["d_new_EasyIvl"] = str(conf['new']['ints'][1])
-    p["d_new_StartingEase"] = conf['new']['initialFactor'] / 10
-    p["d_new_BurySiblings"] = conf['new']['bury']
-    # p["d_new_sep"] = conf['new']["separate"]  # unused
-    p["d_rev_perDay"] = conf['rev']['perDay']
-    p["d_rev_easybonus"] = str(int(100 * conf['rev']['ease4']))
-    p["d_rev_IntMod_int"] = int(100 * conf['rev']['ivlFct'])
-    p["d_rev_IntMod_str"] = str(int(100 * conf['rev']['ivlFct']))
-    p["d_rev_MaxIvl"] = conf['rev']['maxIvl']
-    p["d_rev_BurySiblings"] = conf['rev']['bury']
-    # p["d_rev_minSpace"] = conf['rev']["minSpace"]  # unused
-    # p["d_rev_fuzz"] = conf['rev']["fuzz"]     # unused
-    p["d_lapse_steps"] = conf['lapse']['delays']
-    p["d_lapse_steps_str"] = str(conf['lapse']['delays'])
-    p["d_lapse_NewIvl_int"] = int(100 * conf['lapse']['mult'])
-    p["d_lapse_NewIvl_str"] = str(int(100 * conf['lapse']['mult']))
-    p["d_lapse_MinInt"] = conf['lapse']['minInt']
-    p["d_lapse_LeechThresh"] = conf['lapse']['leechFails']
-    p["d_lapse_LeechAction"] = conf['lapse']['leechAction']
+    p["d_new_order"] = conf.get('new', {}).get('order', "")
+    p["d_new_NewPerDay"] = conf.get('new', {}).get('perDay', "")
+    p["d_new_GradIvl"] = str( conf.get('new', {}).get('ints', ["", ""])[0] )
+    p["d_new_EasyIvl"] = str( conf.get('new', {}).get('ints', ["", ""])[1] )
+    p["d_new_StartingEase"] = conf.get('new', {}).get('initialFactor', 0) / 10
+    p["d_new_BurySiblings"] = conf.get('new', {}).get('bury')
+    p["d_new_sep"] = conf.get('new', {}).get("separate", "")  # unused
+    p["d_rev_perDay"] = conf.get('rev', {}).get('perDay', "")
+    p["d_rev_easybonus"] = str(int(100 * conf.get('rev', {}).get('ease4', 0)))
+    p["d_rev_IntMod_int"] = int(100 * conf.get('rev', {}).get('ivlFct', 0))
+    p["d_rev_IntMod_str"] = str(int(100 * conf.get('rev', {}).get('ivlFct', 0)))
+    p["d_rev_MaxIvl"] = conf.get('rev', {}).get('maxIvl', "")
+    p["d_rev_BurySiblings"] = conf.get('rev', {}).get('bury', "")
+    p["d_rev_minSpace"] = conf.get('rev', {}).get("minSpace", "")  # unused
+    p["d_rev_fuzz"] = conf.get('rev', {}).get("fuzz", "")     # unused
+    p["d_lapse_steps"] = conf.get('lapse', {}).get('delays', "")
+    p["d_lapse_steps_str"] = str(conf.get('lapse', {}).get('delays', ""))
+    p["d_lapse_NewIvl_int"] = int(100 * conf.get('lapse', {}).get('mult', 0))
+    p["d_lapse_NewIvl_str"] = str(int(100 * conf.get('lapse', {}).get('mult', 0)))
+    p["d_lapse_MinInt"] = conf.get('lapse', {}).get('minInt', "")
+    p["d_lapse_LeechThresh"] = conf.get('lapse', {}).get('leechFails', "")
+    p["d_lapse_LeechAction"] = conf.get('lapse', {}).get('leechAction', "")
     return SimpleNamespace(**p)
