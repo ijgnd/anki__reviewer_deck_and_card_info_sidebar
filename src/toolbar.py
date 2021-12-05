c339c7adef8826a16487ce5d08e69900e334ae49
ijgnd <does_not_exist@example.com>
2021-12-05 22:31:04 +0100

    getMenu for qt6

--- src/toolbar.py
+++ src/toolbar.py
@@ -1,15 +1,18 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2020 Lovac42
+# Copyright (c) 2020  Lovac42
+#           (c) 2021- ijgnd
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 
+from aqt import QMenu
+
+
 def get_menu(parent, menu_name):
-    bar = parent.form.menubar
-    for a in bar.actions():
-        if menu_name == a.text():
-            return a.menu()
-    else:
-        return bar.addMenu(menu_name)
+    menubar = parent.form.menubar
+    submenus = menubar.findChildren(QMenu)
+    for s in submenus:
+        if s.title() == menu_name:
+            return s
+    return menubar.addMenu(menu_name)
 
 
 def get_action(parent, action_name):
